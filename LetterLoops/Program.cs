0773135cb2f75bd05f0414b38ca0b34e0861300c
Brooke Lewis <belv2c@gmail.com>
2018-01-20 03:22:24 -0600

    almost finished

--- LetterLoops/Program.cs
+++ LetterLoops/Program.cs
@@ -4,14 +4,46 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+// Write a console application that takes a string from the console input and outputs it as shown below:
+
+// Input       Output
+// abcd        A-Bb-Ccc-Dddd
+// RqaEzty     R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy
+// cwAt        C-Ww-Aaa-Tttt
 namespace LetterLoops
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Type abcd");
-            Console.ReadLine();
+            Console.WriteLine("Type some letters");
+            string letterInput = Console.ReadLine();
+            string finalOutput = "";
+
+            for (int i=0; i < letterInput.Length; i++)
+            {
+                var letters = letterInput[i];
+                var chars = letterInput[i].ToString();
+                string upperCase = chars.ToUpper();
+                string lowerCase = chars.ToLower();
+                if (i == 0)
+                {
+                    finalOutput += $"{upperCase}-";
+                }
+                else if (i == letterInput.Length-1)
+                {
+                    string echo = new String(letters, i);
+                    finalOutput += $"{upperCase}{echo}";
+                }
+                else
+                {
+                    string echo = new String(letters, i);
+                    finalOutput += $"{upperCase}{echo}-";
+                }
+
+                Console.WriteLine(finalOutput);
+                Console.ReadLine();
+            }
         }
     }
 }
